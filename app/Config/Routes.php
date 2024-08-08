ad4e7d698196dcdc0bf4a1488cbd4fc67d1a71a4
VibhoreJain1990 <57622257+VibhoreJain1990@users.noreply.github.com>
2024-08-08 15:26:08 +0530

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,3 +1,6 @@
+
+
+
 <?php
 
 use CodeIgniter\Router\RouteCollection;
@@ -6,7 +9,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->post('user_register','Auth::register');
+
 $routes->post('user_login','Auth::login');
+$routes->options('user_login','Auth::login');
 
 $routes->get('/', 'Home::index');
 $routes->get('client', 'Client::index',['filter' => 'auth']);//vj added the filter to all other routes. 
@@ -18,10 +23,12 @@ $routes->delete('client/(:num)', 'Client::destroy/$1',['filter' => 'auth']);
 //Lets not using auth filter , just a get api to get some data from todos table. 
 //and get it testing using post mann as soon as possible..
 //ofcourse i will create the table using migrations. 
-$routes->get('get_todos','Todos::index');
+$routes->get('get_todos','Todos::index',['filter' => 'auth']);//now its a secure route, 
+$routes->options('get_todos','Todos::index',['filter' => 'auth']);
+//only the authenticated user can view this resource, i mean the person with jwt token(after login). 
 
 $routes->post('create_todo', 'Todos::create');//soon you will be awake by me 
-$routes->options('create_todo', 'Todos::create');
+$routes->options('create_todo', 'Todos::create');//to using options to save ourself from preflight errors. 
 
 //$routes->get('show_todo/(:num)', 'Todos::show/$1');
 
